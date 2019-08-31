94593a6eeb6ca372e1a2496fbf41b1eee36a1088
Konstantin Arapov <konstantin@roxosoft.com>
2019-08-31 10:50:51 +0500

    foo

--- AspNetCoreExp/AspNetCoreExp/Program.cs
+++ AspNetCoreExp/AspNetCoreExp/Program.cs
@@ -13,9 +13,8 @@ namespace AspNetCoreExp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();  
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
