d0fffc2a9ed4125b0fe94250162d8c9b8b6fd2f5
Konstantin Arapov <konstantin@roxosoft.com>
2019-08-31 10:46:39 +0500

    Init

---
+++ AspNetCoreExp/AspNetCoreExp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreExp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
